17bd1cdf1bd9722726a4ef80799d31bc4811a68b
JohanneD <johanne.dybevik@gmail.com>
2023-01-25 15:30:54 +0100

    added delete, and almost made edit

--- src/components/AllTabs/Fridge.js
+++ src/components/AllTabs/Fridge.js
@@ -1,39 +1,24 @@
 import React, { useState } from 'react';
 import InventoryListItem from '../ListItems/inventoryListItem';
 import { foodInventoryFridge } from "./../../data/foodInventoryFridge.js";
-import { toDaysUntil } from './HelperFunc';
 import "./../../styles/Tabs/tabs.css";
 
 function Fridge(){
   const [listInventory, setListInventory] = useState(foodInventoryFridge);
 
-  /*const setUpdate = (updatedText, type, id) => {
-    let newList;
-    if (type === "amount"){
-      console.log(updatedText, id, type)
-      newList = listInventory.map(item => {
-        if (item.id === id) {
-          item.amount = updatedText
-        }
-        return listInventory;
-      })
-    }else if (type === "name"){
-      console.log(updatedText, id, type)
-      newList = listInventory.map(item => {
-        if (item.id === id) {
-          item.name = updatedText
-        }
-        return listInventory;
-      })
-    }else if (type === "expiration"){
-      console.log(updatedText, id, type)
-      newList = listInventory.map(item => {
-        if (item.id === id) {
-          item.expiration = updatedText
-        }
-        return listInventory;
-      })
-    }
+  const setUpdate = (id, name, expiration, amount) => {
+    console.log("in set update");
+    console.log(name, expiration, amount)
+    let newList = listInventory.map(item => {
+      if (item.id === id) {
+        item.name = String(name);
+        console.log(item.name, name)
+        item.expiration = String(expiration);
+        item.amount = String(amount);
+      }
+      return listInventory;
+    })
+    console.log(newList);
     setListInventory(newList);
   }
 
@@ -42,13 +27,13 @@ function Fridge(){
     const newList = listInventory.filter((item) => item.id !== id);
 
     setListInventory(newList);
-  }*/
+  }
 
   return (
     <div className="fridge">
       {listInventory.map((data, key) => {
           return (
-            <InventoryListItem key={key} id={data.id} name={data.name} expiration={toDaysUntil(data.expiration)} amount={data.amount}/>
+            <InventoryListItem key={key} id={data.id} name={data.name} expiration={data.expiration} amount={data.amount} handleDelete={handleDelete} setUpdate={setUpdate}/>
           );
         })}
     </div>
--- src/components/ListItems/inventoryListItem.js
+++ src/components/ListItems/inventoryListItem.js
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./../../styles/inventoryitem.css";
 
-function InventoryListItem({ id, name, expiration, amount }){
+function InventoryListItem({ id, name, expiration, amount, handleDelete, setUpdate}){
+    const [isEditing, setIsEditing] = useState(false);
+    //const [typeEditing, setTypeEditing] = useState("");
+    const [changedName, setChangedName] = useState(name);
+    const [changedExpiration, setChangedExpiration] = useState(expiration);
+    const [changedAmount, setChangedAmount] = useState(amount);
+
+    //let daysToExpiration = toDaysUntil(expiration);
+    console.log("updatedlist");
+
+    const saveEdit = () => {
+        setIsEditing(false);
+        console.log("in save edit");
+        setUpdate(id, changedName, changedExpiration, changedAmount);
+    }
+
+    if (isEditing){
+        return(
+            <div className="inventory_container_edit">
+                <div className="inventory_name">
+                    <input type="text" 
+                    className="textInput" 
+                    value={changedName}
+                    onChange={(e)=>setChangedName(e.target.value)}/>
+                </div>
+                <div className="inventory_expiration">
+                    <input type="text"  
+                        className="textInput" 
+                        value={changedExpiration}
+                        onChange={(e)=>setChangedExpiration(e.target.value)}/>
+                </div>
+                <div className="inventory_amount">
+                    <input type="text"  
+                        className="textInput" 
+                        value={changedAmount}
+                        onChange={(e)=>setChangedAmount(e.target.value)}/>
+                </div>
+                <div className="inventory_input_edit">
+                    <button className='edit_item_button' onClick={() => saveEdit()}>Save</button>
+                </div>
+            </div>
+        );
+    }
+
   return (
       <div className="inventory_container">
-        <div className="inventory_id">
-            <p>{id}</p>
-        </div>
         <div className="inventory_name">
             <p>{name}</p>
         </div>
-        <div className="inventory_expliration">
+        <div className="inventory_expiration">
             <p>{expiration}</p>
         </div>
         <div className="inventory_amount">
             <p>{amount}</p>
         </div>
         <div className="inventory_edit">
-            <button className='edit_item_button'>edit</button>
-            <button className='delete_item_button'>delete</button>
+            <button className='edit_item_button' onClick={() => setIsEditing(true)}>Edit</button>
+            <button className='delete_item_button' onClick={()=>handleDelete(id)}>Delete</button>
         </div>
     </div>
   );
