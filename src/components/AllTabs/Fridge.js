7b03323d813f050da55d2bafe6b203e878e1908d
JohanneD <johanne.dybevik@gmail.com>
2023-01-25 12:17:49 +0100

    added helpfunc for sort and convert expiration

--- src/components/AllTabs/Fridge.js
+++ src/components/AllTabs/Fridge.js
@@ -1,46 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InventoryListItem from '../ListItems/inventoryListItem';
 import { foodInventoryFridge } from "./../../data/foodInventoryFridge.js";
+import { toDaysUntil } from './HelperFunc';
 import "./../../styles/Tabs/tabs.css";
 
-function toDaysUntil(expiration){
-  const current = new Date();
-  var diffDays = "";
-  if (expiration !== ""){
-      const date1 = new Date(expiration.split('/').reverse().join('-'));
-      const date2 = new Date(`${current.getFullYear()}/${current.getMonth()+1}/${current.getDate()}`);
-
-      if (date2.toLocaleDateString("en-US")===date1.toLocaleDateString("en-US")){
-        diffDays = "0";
-      }else{
-        const diffTime = Math.abs(date2 - date1);
-
-        diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-        if (date2.toLocaleDateString("en-US")>=date1.toLocaleDateString("en-US")){
-          diffDays = "-" + diffDays;
-        }
-      }
-      
-  };
-  return diffDays;
-}
-
-
 function Fridge(){
-  //sorting
-  /*var foodFridge = foodInventoryFridge.sort((a, b) => {
+  const [listInventory, setListInventory] = useState(foodInventoryFridge);
 
-    if (toDaysUntil(a.expiration) < toDaysUntil(b.expiration)) {
-      return -1;
+  /*const setUpdate = (updatedText, type, id) => {
+    let newList;
+    if (type === "amount"){
+      console.log(updatedText, id, type)
+      newList = listInventory.map(item => {
+        if (item.id === id) {
+          item.amount = updatedText
+        }
+        return listInventory;
+      })
+    }else if (type === "name"){
+      console.log(updatedText, id, type)
+      newList = listInventory.map(item => {
+        if (item.id === id) {
+          item.name = updatedText
+        }
+        return listInventory;
+      })
+    }else if (type === "expiration"){
+      console.log(updatedText, id, type)
+      newList = listInventory.map(item => {
+        if (item.id === id) {
+          item.expiration = updatedText
+        }
+        return listInventory;
+      })
     }
-    return 1;
-  });*/
+    setListInventory(newList);
+  }
+
+  const handleDelete = (id) => {
+    console.log("delete clicked")
+    const newList = listInventory.filter((item) => item.id !== id);
+
+    setListInventory(newList);
+  }*/
 
   return (
     <div className="fridge">
-      {foodInventoryFridge.map((data, key) => {
+      {listInventory.map((data, key) => {
           return (
-            <InventoryListItem key={key} id={data.id} name={data.name} expiration={toDaysUntil(data.expiration)} amount={data.amount} />
+            <InventoryListItem key={key} id={data.id} name={data.name} expiration={toDaysUntil(data.expiration)} amount={data.amount}/>
           );
         })}
     </div>
---
+++ src/components/AllTabs/HelperFunc.js
@@ -0,0 +1,51 @@
+
+function toDaysUntil(expiration){
+    const current = new Date();
+    var diffDays = "";
+    if (expiration !== ""){
+        const date1 = new Date(expiration.split('/').reverse().join('-'));
+        const date2 = new Date(`${current.getFullYear()}/${current.getMonth()+1}/${current.getDate()}`);
+  
+        if (date2.toLocaleDateString("en-US")===date1.toLocaleDateString("en-US")){
+          diffDays = "0";
+        }else{
+          const diffTime = Math.abs(date2 - date1);
+  
+          diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+          if (date2.toLocaleDateString("en-US")>=date1.toLocaleDateString("en-US")){
+            diffDays = "-" + diffDays;
+          }
+        }
+        
+    };
+    return diffDays;
+  }
+
+
+  function sortInventory(type, list){
+    //sorting
+    var foodFridge = list.sort((a, b) => {
+    if (type === "expiration"){
+        if (toDaysUntil(a.expiration) < toDaysUntil(b.expiration)) {
+            return -1;
+          }
+    }else if(type === "name"){
+        if (a.name < b.name) {
+            return -1;
+          }
+    }else if(type === "amount"){
+        if (a.amount < b.amount) {
+            return -1;
+          }
+    }else if(type === "id"){
+        if (a.id < b.id) {
+            return -1;
+          }
+    }
+    
+    return 1;
+  });
+  return foodFridge;
+  }
+
+  export {toDaysUntil, sortInventory};
